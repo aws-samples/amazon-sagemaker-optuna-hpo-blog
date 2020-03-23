7a900d293d606a9fd35d16689397da4fae2aa9a1
Yoshitaka Haribara <hariby@amazon.com>
2020-03-23 15:48:17 +0000

    use abs path in lifecycle config

--- template/generate-lifecycle-config-base64.py
+++ template/generate-lifecycle-config-base64.py
@@ -1,6 +1,6 @@
 import base64
 content = base64.b64encode(b"""#!/bin/bash
 sudo -u ec2-user -i <<'EOF'
-cd SageMaker && git clone https://github.com/aws-samples/amazon-sagemaker-optuna-hpo-blog.git
+cd /home/ec2-user/SageMaker && git clone https://github.com/aws-samples/amazon-sagemaker-optuna-hpo-blog.git
 EOF""")
 print(content)
